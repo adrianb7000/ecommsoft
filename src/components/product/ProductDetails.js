4313cedc0d51949589bd799ba71c4579aa1268dd
Adrian Betalleluz <abetalleluz@gmail.com>
2022-10-20 21:23:12 -0500

    feat(*) in the Project

---
+++ src/components/product/ProductDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductDetails extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Producto agregado,,, en construccion</h3>
+      </div>
+    )
+  }
+}
